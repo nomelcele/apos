8583f05e6c7b34c46411c6a2052339139a2d8196
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-21 08:32:39 +0000

    g4

---
+++ semi_Pos/src/vo/BonsaVO.java
@@ -0,0 +1,50 @@
+package vo;
+
+public class BonsaVO {
+	private int bon_num;
+	private String bon_name, bon_id, bon_pwd, bon_job, bon_tel, bon_date;
+	public int getBon_num() {
+		return bon_num;
+	}
+	public void setBon_num(int bon_num) {
+		this.bon_num = bon_num;
+	}
+	public String getBon_name() {
+		return bon_name;
+	}
+	public void setBon_name(String bon_name) {
+		this.bon_name = bon_name;
+	}
+	public String getBon_id() {
+		return bon_id;
+	}
+	public void setBon_id(String bon_id) {
+		this.bon_id = bon_id;
+	}
+	public String getBon_pwd() {
+		return bon_pwd;
+	}
+	public void setBon_pwd(String bon_pwd) {
+		this.bon_pwd = bon_pwd;
+	}
+	public String getBon_job() {
+		return bon_job;
+	}
+	public void setBon_job(String bon_job) {
+		this.bon_job = bon_job;
+	}
+	public String getBon_tel() {
+		return bon_tel;
+	}
+	public void setBon_tel(String bon_tel) {
+		this.bon_tel = bon_tel;
+	}
+	public String getBon_date() {
+		return bon_date;
+	}
+	public void setBon_date(String bon_date) {
+		this.bon_date = bon_date;
+	}
+	
+	
+}
