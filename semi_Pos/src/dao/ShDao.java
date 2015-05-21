69354bc279089231985d4a29babb0091658385c7
phoenixcrash <phoenixcrash@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-21 16:10:14 +0000

    q2

--- semi_Pos/src/dao/ShDao.java
+++ semi_Pos/src/dao/ShDao.java
@@ -7,7 +7,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
+import vo.BoardVO;
+import vo.MemVO;
 import conn.MyJndiContext;
 
 public class ShDao {
@@ -19,7 +22,58 @@ public class ShDao {
 		}
 		return dao;
 	}
+	public ArrayList<MemVO> getList(Map<String, Integer> map) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<MemVO> list = new ArrayList<MemVO>();
 
+		StringBuffer sql = new StringBuffer();
+		sql.append("select * from (select rownum r_num, a.* from (");
+		sql.append("select * from member order by mem_NUM desc) a");
+		sql.append(") where r_num between ? and ?");
+		try {
+			con = MyJndiContext.getDs();
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setInt(1, map.get("begin"));
+			pstmt.setInt(2, map.get("end"));
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				MemVO v= new MemVO();
+				v.setMem_num(rs.getInt("mem_num"));
+				v.setMem_name(rs.getString("mem_name"));
+				v.setMem_tel(rs.getString("mem_tel"));
+				v.setMem_addr(rs.getString("mem_addr"));
+				v.setMem_date(rs.getString("mem_date"));
+				//v.setMem_deaddr(rs.getString("mem_deaddr"));
+				//v.setMem
+//				BoardVO v = new BoardVO();
+//				v.setTitle(rs.getString("BO_SUB"));
+//				v.setNo(rs.getInt("BO_NUM"));
+//				v.setWriter(rs.getString("BO_WRITER"));
+//				v.setRegdate(rs.getString("BO_DATE"));
+//				v.setHit(rs.getInt("BO_HIT"));
+//				v.setPath(rs.getString("BO_IMG"));
+//				v.setGroupno(rs.getInt("BO_BONNUM"));
+				list.add(v);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+				if(rs!=null)
+					rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
 	public ArrayList<String> suggestList(String check) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
