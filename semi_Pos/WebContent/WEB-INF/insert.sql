dc53ceb2fbe99e872d0ef44603a40760b9dbd65d
wpdlfhtm <wpdlfhtm@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-22 04:41:27 +0000

    test용

--- semi_Pos/WebContent/WEB-INF/insert.sql
+++ semi_Pos/WebContent/WEB-INF/insert.sql
@@ -15,9 +15,262 @@ shop_seq.nextval,'판교점'
 );
 
 
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',005,1,'test1','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',006,1,'test2','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',007,1,'test3','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',008,1,'test4','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',009,1,'test5','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',010,1,'test6','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',011,1,'test7','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',012,1,'test8','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',013,1,'test9','1234'
+);
+
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',014,1,'test10','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',015,1,'test11','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',016,1,'test12','1234'
+);
+
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',017,1,'test13','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',018,1,'test14','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',019,1,'test15','1234'
+);
+
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',020,1,'test16','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',020,1,'test17','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',020,1,'test18','1234'
+);
+
+
+insert into shop values(
+shop_seq.nextval,'판교점'
+,'032-234-1231','판교',0,0,sysdate,
+'test@naver.com','옷팔아',
+'imagie.img',020,1,'test19','1234'
+);
+
+
+
+
 --mem insert
 insert into member values(
 member_seq.nextval,'test','010-1234-1234','764-1234',
-'성남시 주소 넣어줘','상세주소 넣어줘',0,sysdate,1,'email넣어줘');
+'성남시 주소 넣어줘','우리집 어디야',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test1','010-1234-1234','764-1234',
+'성남시 분당구','우리집 어디게',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test2','010-1234-1234','764-1234',
+'성남시 수지구','수지구 어디게',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test3','010-1234-1234','764-1234',
+'성남시 판교구','판교에 있어요',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test4','010-1234-1234','764-1234',
+'성남시 관악구','라임이 죽여줘',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test5','010-1234-1234','764-1234',
+'성남시 강동구','라임이 쩔지요',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test6','010-1234-1234','764-1234',
+'성남시 강남구','강남구 멋있어',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test7','010-1234-1234','764-1234',
+'성남시 성동구','성동구 성동일',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test8','010-1234-1234','764-1234',
+'성남시 중랑구','중랑구 방랑구',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test9','010-1234-1234','764-1234',
+'성남시 중구','중앙구 중난방',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test10','010-1234-1234','764-1234',
+'성남시 서초구','더이상 할말이',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test11','010-1234-1234','764-1234',
+'성남시 강일구','생각이 힘들어',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test12','010-1234-1234','764-1234',
+'성남시 동동구','머리가 박살나',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test13','010-1234-1234','764-1234',
+'성남시 금천구','더더더 쥐어짜',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test14','010-1234-1234','764-1234',
+'성남시 도봉구','조금만 남았어',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test15','010-1234-1234','764-1234',
+'성남시 성남구','조금만 남았네',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test16','010-1234-1234','764-1234',
+'성남시 주소 넣어줘','얼마안 남았어',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test17','010-1234-1234','764-1234',
+'성남시 우리구','이제곧 고지다',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test18','010-1234-1234','764-1234',
+'성남시 곽한구','어라라 아니네',0,sysdate,1,'email넣어줘');
+
+
+insert into member values(
+member_seq.nextval,'test19','010-1234-1234','764-1234',
+'성남시 도와구','드디어 마지막',0,sysdate,1,'email넣어줘');
+
 
 commit;
