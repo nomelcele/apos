864da838445636088d316542bf3586e20b23ef3c
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-22 01:31:24 +0000

    g1

--- semi_Pos/WebContent/WEB-INF/alter.sql
+++ semi_Pos/WebContent/WEB-INF/alter.sql
@@ -4,3 +4,7 @@ ALTER TABLE comm drop column COMM_EMOTI;
 alter table product MODIFY  pro_name varchar2(50);
 alter table product modify pro_code varchar2(50);
 alter table member add(mem_email varchar2(50)):
+--05.22 ¼öÁ¤
+alter table shop modify shop_adr varchar2(300);
+alter table member modify mem_addr varchar2(200);
+alter table member modify mem_deaddr varchar2(200);
