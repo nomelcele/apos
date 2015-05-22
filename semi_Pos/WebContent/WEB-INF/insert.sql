487d9b53c9d82cbc34596d8c5a0c4b93a8e27942
wpdlfhtm <wpdlfhtm@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-22 06:14:59 +0000

    수정완료

--- semi_Pos/WebContent/WEB-INF/insert.sql
+++ semi_Pos/WebContent/WEB-INF/insert.sql
@@ -183,102 +183,102 @@ insert into shop values(
 
 
 insert into member values(
-member_seq.nextval,'test','010-1234-1234','764-1234',
+1,'test','010-1234-1234','764-1234',
 '성남시 오리구','우리집 어디야',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test1','010-1234-1234','764-1234',
+2,'test1','010-1234-1234','764-1234',
 '성남시 분당구','우리집 어디게',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test2','010-1234-1234','764-1234',
+3,'test2','010-1234-1234','764-1234',
 '성남시 수지구','수지구 어디게',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test3','010-1234-1234','764-1234',
+4,'test3','010-1234-1234','764-1234',
 '성남시 판교구','판교에 있어요',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test4','010-1234-1234','764-1234',
+5,'test4','010-1234-1234','764-1234',
 '성남시 관악구','라임이 죽여줘',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test5','010-1234-1234','764-1234',
+6,'test5','010-1234-1234','764-1234',
 '성남시 강동구','라임이 쩔지요',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test6','010-1234-1234','764-1234',
+7,'test6','010-1234-1234','764-1234',
 '성남시 강남구','강남구 멋있어',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test7','010-1234-1234','764-1234',
+8,'test7','010-1234-1234','764-1234',
 '성남시 성동구','성동구 성동일',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test8','010-1234-1234','764-1234',
+9,'test8','010-1234-1234','764-1234',
 '성남시 중랑구','중랑구 방랑구',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test9','010-1234-1234','764-1234',
+10,'test9','010-1234-1234','764-1234',
 '성남시 중구','중앙구 중난방',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test10','010-1234-1234','764-1234',
+11,'test10','010-1234-1234','764-1234',
 '성남시 서초구','더이상 할말이',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test11','010-1234-1234','764-1234',
+12,'test11','010-1234-1234','764-1234',
 '성남시 강일구','생각이 힘들어',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test12','010-1234-1234','764-1234',
+13,'test12','010-1234-1234','764-1234',
 '성남시 동동구','머리가 박살나',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test13','010-1234-1234','764-1234',
+14,'test13','010-1234-1234','764-1234',
 '성남시 금천구','더더더 쥐어짜',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test14','010-1234-1234','764-1234',
+15,'test14','010-1234-1234','764-1234',
 '성남시 도봉구','조금만 남았어',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test15','010-1234-1234','764-1234',
+16,'test15','010-1234-1234','764-1234',
 '성남시 성남구','조금만 남았네',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test16','010-1234-1234','764-1234',
+17,'test16','010-1234-1234','764-1234',
 '성남시 주소 넣어줘','얼마안 남았어',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test17','010-1234-1234','764-1234',
+18,'test17','010-1234-1234','764-1234',
 '성남시 우리구','이제곧 고지다',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test18','010-1234-1234','764-1234',
+19,'test18','010-1234-1234','764-1234',
 '성남시 곽한구','어라라 아니네',0,sysdate,1,'email넣어줘');
 
 
 insert into member values(
-member_seq.nextval,'test19','010-1234-1234','764-1234',
+20,'test19','010-1234-1234','764-1234',
 '성남시 도와구','드디어 마지막',0,sysdate,1,'email넣어줘');
 
 
