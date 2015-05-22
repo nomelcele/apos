6695e13c1d95b664344fb364ea7a9c762f728c66
phoenixcrash <phoenixcrash@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-22 00:22:17 +0000

    qwe

--- semi_Pos/src/suggest/Suggest.java
+++ semi_Pos/src/suggest/Suggest.java
@@ -19,12 +19,14 @@ public class Suggest {
 	private static Suggest suggest;
 	private Document doc;
 	private Element root;
-	
+
 	public static synchronized Suggest getSuggest() {
-		if(suggest == null)suggest = new Suggest();
+		if (suggest == null)
+			suggest = new Suggest();
 		return suggest;
 	}
-	public Suggest(){
+
+	public Suggest() {
 		SAXBuilder sb = new SAXBuilder();
 		String path = "C:\\kosta97\\semi\\semi_Pos\\WebContent\\shop\\suggest.xml";
 		try {
@@ -34,35 +36,36 @@ public class Suggest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	public String[] getSuggest(String key){
+
+	public String[] getSuggest(String key) {
 		ArrayList<String> sugList = new ArrayList<String>();
 		List<Element> list = new ArrayList<Element>();
-				list=root.getChildren("keyword");
-		System.out.println("key :"+key);
+		list = root.getChildren("keyword");
+		System.out.println("key :" + key);
 		Iterator<Element> it = list.iterator();
-		
+
 		key = key.toLowerCase();
-		while(it.hasNext()){//반복자가 위치한 곳에 다음칸에 요소가 있으면 true
+		while (it.hasNext()) {// 반복자가 위치한 곳에 다음칸에 요소가 있으면 true
 			String result = it.next().getText();
 			String compare = result.toLowerCase();
-			//사용자가 입력한 값으로 시작하는 것인지를 판별 하고 싶다면?	
-			if(compare.startsWith(key)){
-				System.out.println(compare);
+			// 사용자가 입력한 값으로 시작하는 것인지를 판별 하고 싶다면?
+			if (compare.startsWith(key)) {
+				System.out.println("compare:" + compare);
 				sugList.add(result);
 			}
 		}
-		//end while
+		// end while
 		int sugSize = sugList.size();
-		if(sugSize > 0){
+		if (sugSize > 0) {
 			// 어레이 리스트를 배열로 반환
 			String[] sugArr = new String[sugSize];
-			for(String e : sugList){
-				System.out.println("test:"+e);
-				}
+			for (String e : sugList) {
+				System.out.println("test:" + e);
+			}
 			return sugList.toArray(sugArr);
 		}
-			return null;
+		return null;
 	}
 }
