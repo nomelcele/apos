d93452321256e79f0556ae7fc11f4f27fd33e3fb
phoenixcrash <phoenixcrash@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-23 13:09:10 +0000

    ㅂㅈㄷ

--- semi_Pos/src/vo/ProductVO.java
+++ semi_Pos/src/vo/ProductVO.java
@@ -1,51 +1,70 @@
 package vo;
 
 public class ProductVO {
-	private int num, size;
-	private String name, code, barcode,img, date;
-	public int getNum() {
-		return num;
+	private int pro_num, pro_size, pro_price,pro_amount;
+	private String pro_name, pro_code, pro_barcode, pro_img, pro_date, pro_shopname;
+	public int getPro_amount() {
+		return pro_amount;
 	}
-	public void setNum(int num) {
-		this.num = num;
+	public void setPro_amount(int pro_amount) {
+		this.pro_amount = pro_amount;
 	}
-	public int getSize() {
-		return size;
+	public int getPro_num() {
+		return pro_num;
 	}
-	public void setSize(int size) {
-		this.size = size;
+	public void setPro_num(int pro_num) {
+		this.pro_num = pro_num;
 	}
-	public String getName() {
-		return name;
+	public int getPro_size() {
+		return pro_size;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setPro_size(int pro_size) {
+		this.pro_size = pro_size;
 	}
-	public String getCode() {
-		return code;
+	public int getPro_price() {
+		return pro_price;
 	}
-	public void setCode(String code) {
-		this.code = code;
+	public void setPro_price(int pro_price) {
+		this.pro_price = pro_price;
 	}
-	public String getBarcode() {
-		return barcode;
+	public String getPro_name() {
+		return pro_name;
 	}
-	public void setBarcode(String barcode) {
-		this.barcode = barcode;
+	public void setPro_name(String pro_name) {
+		this.pro_name = pro_name;
 	}
-	public String getImg() {
-		return img;
+	public String getPro_code() {
+		return pro_code;
 	}
-	public void setImg(String img) {
-		this.img = img;
+	public void setPro_code(String pro_code) {
+		this.pro_code = pro_code;
 	}
-	public String getDate() {
-		return date;
+	public String getPro_barcode() {
+		return pro_barcode;
 	}
-	public void setDate(String date) {
-		this.date = date;
+	public void setPro_barcode(String pro_barcode) {
+		this.pro_barcode = pro_barcode;
 	}
-	
+	public String getPro_img() {
+		return pro_img;
+	}
+	public void setPro_img(String pro_img) {
+		this.pro_img = pro_img;
+	}
+	public String getPro_date() {
+		return pro_date;
+	}
+	public void setPro_date(String pro_date) {
+		this.pro_date = pro_date;
+	}
+	public String getPro_shopname() {
+		return pro_shopname;
+	}
+	public void setPro_shopname(String pro_shopname) {
+		this.pro_shopname = pro_shopname;
+	}
+
+
 	
 
 }
