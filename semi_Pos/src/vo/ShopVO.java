bc328dd005d97860a2970ea68cffcde6e9602c9a
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-24 11:31:53 +0000

    g2

---
+++ semi_Pos/src/vo/ShopVO.java
@@ -0,0 +1,10 @@
+package vo;
+
+public class ShopVO {
+	// 매장이 가입 할때 필요한 요소
+	// name, tel, adr, date, mail, master img, crnum
+	private String name, tel ,adr, date, mail, master, img, crnum, bon_num, id, pwd;
+	private int num;
+	private float map_x, map_y;
+	
+}
