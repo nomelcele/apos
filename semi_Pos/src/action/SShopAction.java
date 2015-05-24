8521d85e144fcabd64b8699a784975107ebc22f8
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-24 13:17:58 +0000

    g5

--- semi_Pos/src/action/SShopAction.java
+++ semi_Pos/src/action/SShopAction.java
@@ -3,7 +3,7 @@ package action;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import vo.ShopVO;
 import controller.ActionForward;
 
 public class SShopAction implements Action {
@@ -14,6 +14,9 @@ public class SShopAction implements Action {
 		String url="sh_index.jsp";
 		boolean method= false;
 		if(subcmd.equals("shopjoin")){
+			request.setAttribute("hotkey", request.getParameter("hotkey"));
+			request.setAttribute("crnum", request.getParameter("crnum"));
+			System.out.println("여기에서의 crnum"+ request.getParameter("crnum"));
 			url="sh_shopjoin.jsp";
 		}else if(subcmd.equals("shopinsert")){
 			url="";
@@ -23,11 +26,41 @@ public class SShopAction implements Action {
 			String tel1 = request.getParameter("tel1");
 			String tel2 = request.getParameter("tel2");
 			String tel3 = request.getParameter("tel3");
-			String tel = tel1 + tel2 + tel3;
+			String tel = tel1 +'-'+ tel2 +'-'+ tel3;
+			String hotkey = request.getParameter("hotkey");
+			String crnum =  request.getParameter("crnun");
 			// 도로명 주소
 			String addressR =  request.getParameter("adr3_1");
 			// 지번 주소
 			// String addressJ =  request.getParameter("adr4_1");
+			// 위도
+			float lat =  Float.parseFloat(request.getParameter("lat"));
+			// 경도
+			float lng = Float.parseFloat(request.getParameter("lng"));
+			String img = request.getParameter("selfimg");
+			
+			System.out.println("ID : "+id);
+			System.out.println("pwd : "+pwd);
+			System.out.println("EMAIL : "+email);
+			System.out.println("tel : "+tel);
+			System.out.println("ADRRESS : "+addressR);
+			System.out.println("LAT : "+lat);
+			System.out.println("LNG : "+lng);
+			System.out.println("IMG: "+img);
+			System.out.println("CRNUM : "+crnum);
+			System.out.println("HOTKEY : "+hotkey);
+
+			
+			ShopVO vo = new ShopVO();
+			vo.setId(id);
+			vo.setPwd(pwd);
+			vo.setMail(email);
+			vo.setTel(tel);
+			vo.setAdr(addressR);
+			vo.setMap_x(lat);
+			vo.setMap_y(lng);
+			vo.setImg(img);
+			vo.setCrnum(crnum);
 			
 		}
 		return new ActionForward(url, method);
