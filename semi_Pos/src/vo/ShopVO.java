e23674ce4806dbf1cb3b6150b59b775b73495e0b
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-24 11:34:29 +0000

    g1

--- semi_Pos/src/vo/ShopVO.java
+++ semi_Pos/src/vo/ShopVO.java
@@ -6,5 +6,89 @@ public class ShopVO {
 	private String name, tel ,adr, date, mail, master, img, crnum, bon_num, id, pwd;
 	private int num;
 	private float map_x, map_y;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAdr() {
+		return adr;
+	}
+	public void setAdr(String adr) {
+		this.adr = adr;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getMaster() {
+		return master;
+	}
+	public void setMaster(String master) {
+		this.master = master;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getCrnum() {
+		return crnum;
+	}
+	public void setCrnum(String crnum) {
+		this.crnum = crnum;
+	}
+	public String getBon_num() {
+		return bon_num;
+	}
+	public void setBon_num(String bon_num) {
+		this.bon_num = bon_num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public float getMap_x() {
+		return map_x;
+	}
+	public void setMap_x(float map_x) {
+		this.map_x = map_x;
+	}
+	public float getMap_y() {
+		return map_y;
+	}
+	public void setMap_y(float map_y) {
+		this.map_y = map_y;
+	}
 	
 }
