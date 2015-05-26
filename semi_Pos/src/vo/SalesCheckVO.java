5994da0f990b8d8ca65ef6809320c17b4e73337e
wpdlfhtm <wpdlfhtm@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-26 08:32:40 +0000

    vo

---
+++ semi_Pos/src/vo/SalesCheckVO.java
@@ -0,0 +1,29 @@
+package vo;
+
+public class SalesCheckVO {
+	private int count, sell_cash, sell_card;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getSell_cash() {
+		return sell_cash;
+	}
+
+	public void setSell_cash(int sell_cash) {
+		this.sell_cash = sell_cash;
+	}
+
+	public int getSell_card() {
+		return sell_card;
+	}
+
+	public void setSell_card(int sell_card) {
+		this.sell_card = sell_card;
+	}
+}
