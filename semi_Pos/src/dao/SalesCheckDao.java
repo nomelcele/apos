c1a9e94065bc0b4d7b70393b1f949c6dda967758
wpdlfhtm <wpdlfhtm@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-26 08:32:54 +0000

    Dao

---
+++ semi_Pos/src/dao/SalesCheckDao.java
@@ -0,0 +1,66 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import vo.BoardVO;
+import vo.SalesCheckVO;
+import conn.MyJndiContext;
+
+public class SalesCheckDao {
+	private static SalesCheckDao dao;
+
+
+	public static synchronized SalesCheckDao getDao() {
+		if(dao == null)
+			dao = new SalesCheckDao();
+		return dao;
+	}
+	public ArrayList<SalesCheckVO> getList(Map<String, String> map){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<SalesCheckVO> list = new ArrayList<SalesCheckVO>();
+		
+		StringBuffer sql = new StringBuffer();
+		sql.append("select count(*),sum(s.sell_card) sell_card,sum(sell_cash) sell_cash from sell s, product p ");
+		sql.append(" where (p.pro_code like ?) and s.SELL_PRONUM = p.pro_num ");
+		sql.append(" and( sell_date >= DATE ? and sell_Date <= ?);");
+		
+		try {
+			con = MyJndiContext.getDs();
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setString(1, map.get("gender"));
+			pstmt.setString(2, "startdate");
+			pstmt.setString(3, "sysdate");
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				SalesCheckVO v = new SalesCheckVO();
+				v.setSell_card(rs.getInt("sell_card"));
+				v.setSell_cash(rs.getInt("sell_cash"));
+				list.add(v);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+		
+		
+	}
+
+	
+}
