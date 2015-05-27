e2e1a6f0c3752cc16344aeac1593c188893a7d10
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-27 06:08:36 +0000

    drop table

---
+++ semi_Pos/WebContent/WEB-INF/drop.sql
@@ -0,0 +1,14 @@
+-- drop table ¼ø¼­
+drop table stock;
+drop table staff;
+drop table sell;
+drop table comm;
+drop table board;
+drop table chat;
+drop table hotkey;
+drop table loginout;
+drop table discount;
+drop table member;
+drop table shop;
+drop table product;
+drop table bonsa;
