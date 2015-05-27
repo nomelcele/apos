c25b24d306b6dca95e6fa7bf045eb5d55df3eaf9
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-27 06:09:29 +0000

    g1

--- semi_Pos/src/action/SShopAction.java
+++ semi_Pos/src/action/SShopAction.java
@@ -23,48 +23,50 @@ public class SShopAction implements Action {
 			System.out.println("여기에서의 crnum"+ request.getParameter("crnum"));
 			url="sh_shopjoin.jsp";
 		}else if(subcmd.equals("shopinsert")){
-			url="";
-			String id = request.getParameter("id");
-			String pwd = request.getParameter("pwd");
-			String email= request.getParameter("email");
+			url="sh_login.jsp";
+			// shop_num, shop_name, shop_tel, shop_adr,shop_map_x, shop_map_y
+			// shop_date, shop_mail, shop_master, shop_img, shop_crnum, shop_bonnum
+			// shop_id, shop_pwd
+			
+			String name = request.getParameter("shopname");
 			String tel1 = request.getParameter("tel1");
 			String tel2 = request.getParameter("tel2");
 			String tel3 = request.getParameter("tel3");
-			String tel = tel1 +'-'+ tel2 +'-'+ tel3;
-			String hotkey = request.getParameter("hotkey");
-			String crnum =  request.getParameter("crnun");
-			// 도로명 주소
-			String addressR =  request.getParameter("adr3_1");
-			// 지번 주소
-			// String addressJ =  request.getParameter("adr4_1");
-			// 위도
-			float lat =  Float.parseFloat(request.getParameter("lat"));
-			// 경도
+			String tel = tel1+"-"+tel2+"-"+tel3;
+			String adr = request.getParameter("adr3_1");
+			float lat = Float.parseFloat(request.getParameter("lat"));
 			float lng = Float.parseFloat(request.getParameter("lng"));
+			String mail = request.getParameter("email");
+			String master = request.getParameter("name");
 			String img = request.getParameter("selfimg");
+			int crnum =Integer.parseInt(request.getParameter("crnum"));
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
 			
 			System.out.println("ID : "+id);
 			System.out.println("pwd : "+pwd);
-			System.out.println("EMAIL : "+email);
+			System.out.println("EMAIL : "+mail);
 			System.out.println("tel : "+tel);
-			System.out.println("ADRRESS : "+addressR);
+			System.out.println("ADRRESS : "+adr);
 			System.out.println("LAT : "+lat);
 			System.out.println("LNG : "+lng);
 			System.out.println("IMG: "+img);
 			System.out.println("CRNUM : "+crnum);
-			System.out.println("HOTKEY : "+hotkey);
 
 			
 			ShopVO vo = new ShopVO();
-			vo.setId(id);
-			vo.setPwd(pwd);
-			vo.setMail(email);
+			vo.setName(name);
 			vo.setTel(tel);
-			vo.setAdr(addressR);
+			vo.setAdr(adr);
 			vo.setMap_x(lat);
 			vo.setMap_y(lng);
+			vo.setMail(mail);
+			vo.setMaster(master);
 			vo.setImg(img);
 			vo.setCrnum(crnum);
+			vo.setId(id);
+			vo.setPwd(pwd);
+			
 			
 			ShopDao.getDao().shopjoin(vo);
 			
