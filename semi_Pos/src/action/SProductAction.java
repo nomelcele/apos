d5b4ab28f53dba29347a49ee8d4c9033d0507d4e
phoenixcrash <phoenixcrash@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-29 13:05:32 +0000

    qwe

--- semi_Pos/src/action/SProductAction.java
+++ semi_Pos/src/action/SProductAction.java
@@ -29,6 +29,13 @@ public class SProductAction implements Action{
 				ArrayList<SmangVO>list = ShDao.getDao().getListProduct(name,shopnum);
 				request.setAttribute("list", list);
 				System.out.println("test");
+			}else if(childcmd != null && childcmd.equals("search_top")){
+				String name =request.getParameter("top_name_pk");
+				String shopnum= request.getParameter("top_shop_num");
+				System.out.println("test"+name);
+				ArrayList<SmangVO>list = ShDao.getDao().getListProduct(name,shopnum);
+				request.setAttribute("list", list);
+				System.out.println("test");
 			}
 			url="sh_productCheck.jsp";
 		}else if(subcmd.equals("move")){
