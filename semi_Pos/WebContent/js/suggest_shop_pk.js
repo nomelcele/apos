0ee76d4b4d7194e4d332319769d4c63b6d7a1dce
phoenixcrash <phoenixcrash@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-05-30 01:16:54 +0000

    qwe

---
+++ semi_Pos/WebContent/js/suggest_shop_pk.js
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+	var lastKey = '';
+	var check = false;
+	var loopKey = false;
+
+	function startSuggest() {
+		
+		if (check == false) {
+			setTimeout("sendKeyword();", 500);
+			loopKey = true;
+		}
+		check = true;
+	}
+	function sendKeyword() {
+		
+		if (loopKey == false) {
+			return;
+		}
+		var key = shop_name.value;
+		if (key == '' || key == ' ') {
+			lastKey = '';
+			document.getElementById("view").style.display = 'none';
+		} else if (key != lastKey) {
+			lastKey = key;
+			var param = "key=" + encodeURIComponent(key);
+			sendRequest("../bonsa/bon_ajaxSuggestShop.jsp", param, res, "post");
+		}
+		setTimeout("sendKeyword();", 1000);
+	}
+
+	var jsonObj = null;
+	function res() {
+
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				var response = xhr.responseText;
+				jsonObj = JSON.parse(response);
+				viewTablesh();
+			} else {
+				document.getElementById("view").style.display = 'none';
+			}
+		}
+	}
+	function viewTablesh() {
+		var vD = document.getElementById("view");
+		var htmlTxt = "<table>";
+		for (var i = 0; i < jsonObj.length; i++) {
+			htmlTxt += "<tr><td style='cursor:pointer;'onmouseover='this.style.background=\"silver\"'onmouseout='this.style.background=\"white\"' onclick='select("
+					+ i + ")'>" + jsonObj[i] + "</td></tr>";
+		}
+		htmlTxt += "</table>";
+		vD.innerHTML = htmlTxt;
+		vD.style.display = "block";
+	}
+	function select(index) {
+		shop_name.value = jsonObj[index];
+		document.getElementById("view").style.display = 'none';
+		check = false;
+		loopKey = false;
+	}
