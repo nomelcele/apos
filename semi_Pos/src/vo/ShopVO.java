af37c717069af92673fb35b0ff491eb7844fac84
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-06-08 10:12:30 +0000

    g1

--- semi_Pos/src/vo/ShopVO.java
+++ semi_Pos/src/vo/ShopVO.java
@@ -3,92 +3,91 @@ package vo;
 public class ShopVO {
 	// 매장이 가입 할때 필요한 요소
 	// name, tel, adr, date, mail, master img, crnum
-	private String name, tel ,adr, date, mail, master, img, id, pwd;
-	private int num, bon_num, crnum;
-	private float map_x, map_y;
-	public String getName() {
-		return name;
+	private String shop_name, shop_tel ,shop_adr, shop_date, shop_mail, shop_master, shop_img, shop_id, shop_pwd;
+	private int shop_num, shop_bonnum, shop_crnum;
+	private float shop_map_x, shop_map_y;
+	public String getShop_name() {
+		return shop_name;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setShop_name(String shop_name) {
+		this.shop_name = shop_name;
 	}
-	public String getTel() {
-		return tel;
+	public String getShop_tel() {
+		return shop_tel;
 	}
-	public void setTel(String tel) {
-		this.tel = tel;
+	public void setShop_tel(String shop_tel) {
+		this.shop_tel = shop_tel;
 	}
-	public String getAdr() {
-		return adr;
+	public String getShop_adr() {
+		return shop_adr;
 	}
-	public void setAdr(String adr) {
-		this.adr = adr;
+	public void setShop_adr(String shop_adr) {
+		this.shop_adr = shop_adr;
 	}
-	public String getDate() {
-		return date;
+	public String getShop_date() {
+		return shop_date;
 	}
-	public void setDate(String date) {
-		this.date = date;
+	public void setShop_date(String shop_date) {
+		this.shop_date = shop_date;
 	}
-	public String getMail() {
-		return mail;
+	public String getShop_mail() {
+		return shop_mail;
 	}
-	public void setMail(String mail) {
-		this.mail = mail;
+	public void setShop_mail(String shop_mail) {
+		this.shop_mail = shop_mail;
 	}
-	public String getMaster() {
-		return master;
+	public String getShop_master() {
+		return shop_master;
 	}
-	public void setMaster(String master) {
-		this.master = master;
+	public void setShop_master(String shop_master) {
+		this.shop_master = shop_master;
 	}
-	public String getImg() {
-		return img;
+	public String getShop_img() {
+		return shop_img;
 	}
-	public void setImg(String img) {
-		this.img = img;
+	public void setShop_img(String shop_img) {
+		this.shop_img = shop_img;
 	}
-	public int getCrnum() {
-		return crnum;
+	public String getShop_id() {
+		return shop_id;
 	}
-	public void setCrnum(int crnum) {
-		this.crnum = crnum;
+	public void setShop_id(String shop_id) {
+		this.shop_id = shop_id;
 	}
-	public int getBon_num() {
-		return bon_num;
+	public String getShop_pwd() {
+		return shop_pwd;
 	}
-	public void setBon_num(int bon_num) {
-		this.bon_num = bon_num;
+	public void setShop_pwd(String shop_pwd) {
+		this.shop_pwd = shop_pwd;
 	}
-	public String getId() {
-		return id;
+	public int getShop_num() {
+		return shop_num;
 	}
-	public void setId(String id) {
-		this.id = id;
+	public void setShop_num(int shop_num) {
+		this.shop_num = shop_num;
 	}
-	public String getPwd() {
-		return pwd;
+	public int getShop_bonnum() {
+		return shop_bonnum;
 	}
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
+	public void setShop_bonnum(int shop_bonnum) {
+		this.shop_bonnum = shop_bonnum;
 	}
-	public int getNum() {
-		return num;
+	public int getShop_crnum() {
+		return shop_crnum;
 	}
-	public void setNum(int num) {
-		this.num = num;
+	public void setShop_crnum(int shop_crnum) {
+		this.shop_crnum = shop_crnum;
 	}
-	public float getMap_x() {
-		return map_x;
+	public float getShop_map_x() {
+		return shop_map_x;
 	}
-	public void setMap_x(float map_x) {
-		this.map_x = map_x;
+	public void setShop_map_x(float shop_map_x) {
+		this.shop_map_x = shop_map_x;
 	}
-	public float getMap_y() {
-		return map_y;
+	public float getShop_map_y() {
+		return shop_map_y;
 	}
-	public void setMap_y(float map_y) {
-		this.map_y = map_y;
+	public void setShop_map_y(float shop_map_y) {
+		this.shop_map_y = shop_map_y;
 	}
-	
 }
