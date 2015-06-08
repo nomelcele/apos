6ae2a1722899a19d68972ba9e54fe01934d297a7
lifeveril <lifeveril@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-06-08 11:00:45 +0000

    123123

--- semi_Pos/src/dao/ShDao.java
+++ semi_Pos/src/dao/ShDao.java
@@ -14,6 +14,11 @@ import java.util.Map;
 
 
 
+
+
+import org.apache.ibatis.session.SqlSession;
+
+import service.FactoryService;
 import vo.BoardVO;
 import vo.MemVO;
 import vo.ProductVO;
@@ -256,44 +261,10 @@ public class ShDao {
 		return list;
 	}
 
-	public void insertMem(HashMap<String, String> map) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		System.out.println("확인");
-		try {
-			con = MyJndiContext.getDs();
-			StringBuffer sql = new StringBuffer();
-			StringBuffer post = new StringBuffer();
-			StringBuffer tel = new StringBuffer();
-			sql.append("insert into member values(");
-			sql.append("member_seq.nextval,?,?,?,?,?,0,sysdate,1,?)");
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, map.get("name"));
-			tel.append(map.get("tel1")).append("-").append(map.get("tel2")).append("-").append(map.get("tel3"));
-			pstmt.setString(2, tel.toString());
-			post.append(map.get("adr1")).append("-").append(map.get("adr2"));
-			pstmt.setString(3, post.toString());
-			pstmt.setString(4, map.get("adr3"));
-			pstmt.setString(5, map.get("adr4"));
-			// 나중에 세션처리 구현 하면 넣어야되 회원이 가입한 상점번호
-			// pstmt.setString(6,map.get("shopnum"));
-			pstmt.setString(6, map.get("email"));
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (con != null)
-					con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
+	public void insertMem(MemVO vo) {
+		SqlSession ss = FactoryService.getFactory().openSession(true);
+		ss.insert("member.mem_in",vo);
+		ss.close();
 	}
 	
 	public MemVO getDetail(int no){
