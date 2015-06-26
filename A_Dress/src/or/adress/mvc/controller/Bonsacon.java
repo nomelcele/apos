10fd8aab85b8842987a31f880a8b5f5c7684a1c3
lifeveril <lifeveril@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-06-26 08:54:15 +0000

    0626경연

--- A_Dress/src/or/adress/mvc/controller/Bonsacon.java
+++ A_Dress/src/or/adress/mvc/controller/Bonsacon.java
@@ -6,10 +6,60 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Bonsacon {
-@RequestMapping(value="/bon_index")
-public ModelAndView bon_index(){
-	ModelAndView mav= new ModelAndView();
-	mav.setViewName("bonsa/bon_index");
-	return mav;
-}
+	@RequestMapping(value = "/bon_index")
+	public ModelAndView bon_index() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("bonsa/bon_index");
+		return mav;
+	}
+	// 본사 업무관리 공지사항
+	@RequestMapping(value = "/bon_workNotice")
+	public ModelAndView bon_workNotice() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("bonsa/bon_workNotice");
+		return mav;
+	}
+	// 본사 매장관리 매장가입
+	@RequestMapping(value = "/bon_shopJoin")
+	public ModelAndView bon_shopJoin() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("bonsa/bon_shopJoin");
+		return mav;
+	}
+	// 본사 매장관리 매장조회
+	@RequestMapping(value = "/bon_shopCheck")
+	public ModelAndView bon_shopCheck() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("bonsa/bon_shopCheck");
+		return mav;
+	}
+	// 본사 상품관리 상품추가
+	@RequestMapping(value = "/bon_productAdd")
+	public ModelAndView bon_productAdd() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("bonsa/bon_productAdd");
+		return mav;
+	}
+	// 본사 상품관리 상품재고관리
+	@RequestMapping(value = "/bon_productSale")
+	public ModelAndView bon_productSale() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("bonsa/bon_productSale");
+		return mav;
+	}
+	// bon_termSalesCheck 품목별 매출순위
+		@RequestMapping(value = "/bon_termSalesCheck")
+		public ModelAndView bon_termSalesCheck() {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("bonsa/bon_termSalesCheck2");
+			return mav;
+		}
+	
+	// bon_productSalesCheck 대리점별 매출순위
+	@RequestMapping(value = "/bon_productSalesCheck")
+	public ModelAndView bon_productSalesCheck() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("bonsa/bon_productSalesCheck");
+		return mav;
+	}
 }
