6f1fae64e55ac64a0f701d3ae87a72a0ae723796
wpdlfhtm <wpdlfhtm@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-07-02 01:02:25 +0000

    유후

--- A_Dress/src/or/adress/mvc/dao/SalesCheckDao.java
+++ A_Dress/src/or/adress/mvc/dao/SalesCheckDao.java
@@ -98,6 +98,7 @@ public class SalesCheckDao {
 		
 	
 	// 인자값 3개.. 물어볼 것
+	// 완료 확인요망
 	public List<SalesCheckVO> getList(String shopname, String startdate,
 			String enddate) {
 		
@@ -107,54 +108,6 @@ public class SalesCheckDao {
 		map.put("enddate", enddate);
 		
 		return ss.selectList("salescheck.salescheck_list", map);
-		
-		
-		
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		ArrayList<SalesCheckVO> list = new ArrayList<SalesCheckVO>();
-		System.out.println("map" + shopname);
-		System.out.println("gender" + startdate);
-
-		StringBuffer sql = new StringBuffer();
-		sql.append("select  to_char(sell_date,'yyyy-MM') sedate,count(*) cnt ");
-		sql.append(" ,sum(s.sell_cash) sell_cash, sh.SHOP_NAME sell_shopname ");
-		sql.append(" from sell s, shop sh  where s.SELL_SHOPNUM = sh.shop_num and sh.SHOP_NAME like #{shopname}  ");
-		sql.append(" and( sell_date >= to_date(#{startdate},'yyyy-MM-dd')  and sell_Date <= to_date(#{enddate},'yyyy-MM-dd')) ");
-		sql.append("  group by to_char(sell_date,'yyyy-MM') ,sh.SHOP_NAME ORDER BY 1");
-
-		try {
-			con = MyJndiContext.getDs();
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, shopname);
-			pstmt.setString(2, startdate);
-			pstmt.setString(3, enddate);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				SalesCheckVO v = new SalesCheckVO();
-				System.out.println("test34" + rs.getInt("sell_cash"));
-				v.setDate(rs.getString("sedate"));
-				v.setSell_cash(rs.getInt("sell_cash"));
-				v.setSell_shopname(rs.getString("sell_shopname"));
-				v.setCount(rs.getInt("cnt"));
-				list.add(v);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (con != null)
-					con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
 
 	}
 	
