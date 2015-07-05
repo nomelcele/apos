add15c342c76f2c7b76d306f50ff6d781a9c1aff
honggi31 <honggi31@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-07-05 07:44:55 +0000

    주말노동1

--- A_Dress/src/or/adress/mvc/controller/Ajaxcon.java
+++ A_Dress/src/or/adress/mvc/controller/Ajaxcon.java
@@ -1,11 +1,26 @@
 package or.adress.mvc.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import or.adress.mvc.dao.BonsaDao;
 import or.adress.mvc.dao.ProductDao;
 import or.adress.mvc.dao.ShopDao;
 import or.adress.mvc.dao.SmangDao;
@@ -29,6 +44,8 @@ public class Ajaxcon {
 	private SmangDao smdao;
 	@Autowired
 	private ShopDao shdao;
+	@Autowired
+	private BonsaDao bdao;
 	//성별 물건 검색 ajax
 	@RequestMapping(value = "sh_AjaxProductSearch")
 	public ModelAndView sh_AjaxProductSearch(String shop_num,
@@ -152,10 +169,216 @@ public class Ajaxcon {
 		return mav;
 	}
 	
-	@RequestMapping(value="bon_checkcaptcha")
-	public ModelAndView bon_checkcaptcha(){
+	//본사로그인 캡챠 (로그인)
+	@RequestMapping(value="bon_checkcaptcha", method=RequestMethod.POST)
+	public ModelAndView bon_checkcaptcha(String id, String pwd){
+		ModelAndView mav = new ModelAndView("ajax/bon_checkcaptcha");
+//		String id = request.getParameter("id");
+//		String pwd = request.getParameter("pwd");
 		
+
+		System.out.println("-----------bon_checkcaptcha---------");
+		System.out.println("Request ID : " + id);
+		System.out.println("Request PWD : " + pwd);
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("id", id);
+		map.put("pwd", pwd);
+		String resid = bdao.logincheck(map);
+		boolean res = false;
+		if(id.equals(resid)){
+			res=true;
+		}
+		System.out.println("Request RES : " + res);
+		mav.addObject("res", res);
+		return mav;
 	}
+	
+	//아이디찾기 (디비)
+	@RequestMapping(value="bon_findid", method=RequestMethod.POST)
+	public ModelAndView bon_findid(String name, String tel1, String tel2, String tel3){
+		ModelAndView mav = new ModelAndView("ajax/bon_findid");
+//		String name = request.getParameter("name");
+//		String tel1 = request.getParameter("tel1");
+//		String tel2 = request.getParameter("tel2");
+//		String tel3 = request.getParameter("tel3");
+		String tel = tel1+"-"+tel2+"-"+tel3;
+
+		System.out.println("-----------bon_findid.jsp---------");
+		System.out.println("Request NAME : " + name);
+		System.out.println("Request TEL : " + tel);
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", name);
+		map.put("tel", tel);
+		String res = bdao.findid(map);
+		System.out.println("Request RES : " + res);
+		mav.addObject("res", res);
+		return mav;
+	}
+	
+	//아이디 찾기 메일발송
+	@RequestMapping(value="bon_findid2", method=RequestMethod.POST)
+	public void bon_findid2(HttpServletRequest request){
+		// 메일 관련 정보
+		String mail = request.getParameter("mail");
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		
+		System.out.println("--------여기는 FIND ID 메일 보내는 곳--------");
+		System.out.println("MAIL : "+mail);
+		System.out.println("NAME : "+name);
+		System.out.println("ID : "+id);
+		
+		String host = "smtp.naver.com";
+        final String username = "ama949@naver.com"; // 보내는 사람 네이버  ID
+        final String password = "skdltm11a"; // 비밀번호
+        int port=465;
+         
+        // 메일 내용
+        String recipient = mail; // 받는 사람 E-Mail
+        String subject = "APOS - ID 찾기 결과입니다.";
+        String body = "\""+name+"\"님의" +"\n\r"+"ID는 : "+id+"\n\r"+"감사합니다";
+         
+        Properties props = System.getProperties();
+         
+         
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.starttls.enable","true");  
+        props.put("mail.smtp.ssl.trust", host);
+          
+        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+            String un=username;
+            String pw=password;
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(un, pw);
+            }
+        });
+        session.setDebug(true); //for debug
+          
+        Message msg = new MimeMessage(session);
+        try {
+			msg.setFrom(new InternetAddress(username));
+			msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+			msg.setSubject(subject);
+			msg.setSentDate(new Date());
+			// 파일 첨부시에는 BodyPart를 사용
+			// msg.setText(body);
+			
+			// 파일첨부를 위한 Multipart
+			Multipart multipart = new MimeMultipart();
+			
+			// BodyPart를 생성
+			BodyPart bodyPart = new MimeBodyPart();
+			bodyPart.setText(body);
+			
+			// 1. Multipart에 BodyPart를 붙인다.
+			multipart.addBodyPart(bodyPart);
+			
+			// 이메일 메시지의 내용에 Multipart를 붙인다.
+			msg.setContent(multipart);
+			Transport.send(msg);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// 비밀번호찾기 (디비)
+	@RequestMapping(value="bon_findpwd", method=RequestMethod.POST)
+	public ModelAndView bon_findpwd(String name, String id){
+//		String name = request.getParameter("name");
+//		String id = request.getParameter("id");
+
+		System.out.println("-----------bon_findpwd.jsp---------");
+		System.out.println("Request NAME : " + name);
+		System.out.println("Request ID : " + id);
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", name);
+		map.put("id", id);
+		String res = bdao.findpwd(map);
+		System.out.println("Request RES : " + res);
+		ModelAndView mav = new ModelAndView("ajax/bon_findpwd");
+		mav.addObject("res", res);
+		return mav;
+	}
+	
+	//비밀번호 찾기 메일발송
+	@RequestMapping(value="bon_findpwd2", method=RequestMethod.POST)
+	public void bon_findpwd2(String name, String mail, String pwd){
+		// 메일 관련 정보
+//					String mail = request.getParameter("mail");
+//					String name = request.getParameter("name");
+//					String pwd = request.getParameter("pwd");
+					
+					System.out.println("--------여기는 FIND ID 메일 보내는 곳--------");
+					System.out.println("MAIL : "+mail);
+					System.out.println("NAME : "+name);
+					System.out.println("PWD : "+pwd);
+					
+					String host = "smtp.naver.com";
+			        final String username = "ama949@naver.com"; // 보내는 사람 네이버  ID
+			        final String password = "skdltm11a"; // 비밀번호
+			        int port=465;
+			         
+			        // 메일 내용
+			        String recipient = mail; // 받는 사람 E-Mail
+			        String subject = "APOS - PASS WORD 찾기 결과입니다.";
+			        String body = "\""+name+"\"님의" +"\n\r"+"PWD는 : "+pwd+"\n\r"+"감사합니다";
+			         
+			        Properties props = System.getProperties();
+			         
+			         
+			        props.put("mail.smtp.host", host);
+			        props.put("mail.smtp.port", port);
+			        props.put("mail.smtp.auth", "true");
+			        props.put("mail.smtp.ssl.enable", "true");
+			        props.put("mail.smtp.starttls.enable","true");  
+			        props.put("mail.smtp.ssl.trust", host);
+			          
+			        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			            String un=username;
+			            String pw=password;
+			            protected PasswordAuthentication getPasswordAuthentication() {
+			                return new PasswordAuthentication(un, pw);
+			            }
+			        });
+			        session.setDebug(true); //for debug
+			          
+			        Message msg = new MimeMessage(session);
+			        try {
+						msg.setFrom(new InternetAddress(username));
+						msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+						msg.setSubject(subject);
+						msg.setSentDate(new Date());
+						// 파일 첨부시에는 BodyPart를 사용
+						// msg.setText(body);
+						
+						// 파일첨부를 위한 Multipart
+						Multipart multipart = new MimeMultipart();
+						
+						// BodyPart를 생성
+						BodyPart bodyPart = new MimeBodyPart();
+						bodyPart.setText(body);
+						
+						// 1. Multipart에 BodyPart를 붙인다.
+						multipart.addBodyPart(bodyPart);
+						
+						// 이메일 메시지의 내용에 Multipart를 붙인다.
+						msg.setContent(multipart);
+						Transport.send(msg);
+					} catch (MessagingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	}
+	
+	
+	
+	
 	
 	@RequestMapping(value="sh_ajaxsaletable")
 	public ModelAndView sh_ajaxsaletable(){
--- A_Dress/src/or/adress/mvc/controller/Logincon.java
+++ A_Dress/src/or/adress/mvc/controller/Logincon.java
@@ -1,11 +1,28 @@
 package or.adress.mvc.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import or.adress.mvc.dao.BonsaDao;
+import or.adress.mvc.dao.LogTimeDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+import vo.BonsaVO;
 
 @Controller
 public class Logincon {
+	@Autowired
+	private BonsaDao bdao;
+	@Autowired
+	private LogTimeDao ldao;
 	
 	@RequestMapping(value="/bon_login")
 	public ModelAndView bon_login(){
@@ -18,4 +35,66 @@ public class Logincon {
 		ModelAndView mav = new ModelAndView("login/sh_login");
 		return mav;
 	}
+	
+	@RequestMapping(value="bon_findidform", method=RequestMethod.POST)
+	public ModelAndView bon_findidform(){
+		
+		return bon_login();
+	}
+	
+	@RequestMapping(value="bon_findpwdform", method=RequestMethod.POST)
+	public ModelAndView bon_findpwdform(){
+		
+		return bon_login();
+	}
+	
+	// 본사 - 사원 가입
+	@RequestMapping(value="bon_sawonjoin", method=RequestMethod.POST)
+	public ModelAndView bon_sawonjoin(HttpServletRequest request){
+		
+				// 이름 ,비밀번호, 비밀번호 확인, 성명, 연락처
+				
+				String bon_id = request.getParameter("bon_id");
+				String bon_name = request.getParameter("bon_name");
+				String bon_pwd = request.getParameter("bon_pwd");
+				String tel1 = request.getParameter("bon_tel1");
+				String tel2 = request.getParameter("bon_tel2");
+				String tel3 = request.getParameter("bon_tel3");
+				String bon_tel = tel1 +"-"+ tel2+"-"+tel3;
+				
+				BonsaVO vo = new BonsaVO();
+				vo.setBon_id(bon_id);
+				vo.setBon_name(bon_name);
+				vo.setBon_pwd(bon_pwd);
+				vo.setBon_tel(bon_tel);
+				bdao.sawonjoin(vo);
+		return bon_login();
+	}
+	
+	
+	//본사로그인 (화면전환, 세션저장) (아이디/비번체크는 캡챠에서)
+	@RequestMapping(value="bon_sawonlogin", method=RequestMethod.POST)
+	public ModelAndView bon_sawonlogin(HttpServletRequest request){
+		String id =request.getParameter("bon_login_id");
+		String bon_name = bdao.getbonname(id);
+		ldao.logintime(bdao.getbonnum(id)); // 로그인 시간 DB에 저장
+		HttpSession session = request.getSession();
+		session.setAttribute("bon_id", id);
+		session.setAttribute("bon_name", bon_name);
+		ModelAndView mav = new ModelAndView("bonsa/bon_index");
+		return mav;
+	}
+	
+	//본사로그아웃
+	@RequestMapping(value="bon_sawonlogout")
+	public ModelAndView bon_sawonlogout(HttpSession session){
+		// 본사 사원 로그 아웃
+					String bon_id =(String) session.getAttribute("bon_id");
+					ldao.logouttime(bdao.getbonnum(bon_id));
+					session.invalidate(); //모든 세션삭제
+					return bon_login();
+	}
+	
+	
+	
 }
