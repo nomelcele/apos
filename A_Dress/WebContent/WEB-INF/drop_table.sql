579d96e2b4362b5e321a64e51bc5663ff0ebed6b
lifeveril <lifeveril@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-07-09 00:40:22 +0000

    qwe

---
+++ A_Dress/WebContent/WEB-INF/drop_table.sql
@@ -0,0 +1,15 @@
+drop table bonsa cascade constraint;
+drop table stock cascade constraint;
+drop table shop cascade constraint;
+drop table sell cascade constraint;
+drop table member cascade constraint;
+drop table board cascade constraint;
+drop table comm cascade constraint;
+drop table chat cascade constraint;
+drop table loginout cascade constraint;
+drop table product cascade constraint;
+drop table hotkey cascade constraint;
+drop table discount cascade constraint;
+drop table staff cascade constraint;
+drop table croom cascade constraint;
+drop table stockreq cascade constraint;
