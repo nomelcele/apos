8b85a88dbed26819a534b0599b4b6e05b7bb34d1
wpdlfhtm <wpdlfhtm@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-07-09 01:15:13 +0000

    시퀀스 커밋

--- A_Dress/WebContent/WEB-INF/insert.sql
+++ A_Dress/WebContent/WEB-INF/insert.sql
@@ -1511,6 +1511,42 @@ insert into member values(member_seq.nextval,'나영석','010-5464-8747','235-1234'
 
 
 
+-- 테이블 시퀀스 모음
+Create sequence bonsa_seq
+increment by 1 start with 1;
+
+Create sequence staff_seq
+increment by 1 start with 1;
+
+Create sequence member_seq
+increment by 1 start with 1;
+
+Create sequence board_seq
+increment by 1 start with 1;
+
+Create sequence comm_seq
+increment by 1 start with 1;
+
+Create sequence chat_seq
+increment by 1 start with 1;
+
+Create sequence loginout_seq
+increment by 1 start with 1;
+
+Create sequence product_seq
+increment by 1 start with 1;
+
+Create sequence sell_seq
+increment by 1 start with 1;
+
+Create sequence hotkey_seq
+increment by 1 start with 1;
+
+Create sequence stock_seq
+increment by 1 start with 1;
+
+Create sequence staff_seq
+increment by 1 start with 1;
 
 
 commit;
