f8b8c3e4a2225b18e0043e333d1a75e364a361d0
lifeveril <lifeveril@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-07-09 02:02:20 +0000

    qwe

---
+++ A_Dress/WebContent/WEB-INF/sequence.sql
@@ -0,0 +1,64 @@
+
+drop sequence bonsa_seq;
+drop sequence member_seq;
+drop sequence shop_seq;
+drop sequence board_seq;
+drop sequence comm_seq;
+drop sequence chat_seq;
+drop sequence loginout_seq;
+drop sequence product_seq;
+drop sequence sell_seq;
+drop sequence hotkey_seq;
+drop sequence stock_seq;
+drop sequence discount_seq;
+drop sequence staff_seq;
+drop sequence stockreq_seq;
+drop sequence sroot_seq;
+
+-- 테이블 시퀀스 모음
+Create sequence bonsa_seq
+increment by 1 start with 1;
+
+Create sequence member_seq
+increment by 1 start with 1;
+
+Create sequence shop_seq
+increment by 1 start with 1;
+
+Create sequence board_seq
+increment by 1 start with 1;
+
+Create sequence comm_seq
+increment by 1 start with 1;
+
+Create sequence chat_seq
+increment by 1 start with 1;
+
+Create sequence loginout_seq
+increment by 1 start with 1;
+
+Create sequence product_seq
+increment by 1 start with 1;
+
+Create sequence sell_seq
+increment by 1 start with 1;
+
+Create sequence hotkey_seq
+increment by 1 start with 1;
+
+Create sequence stock_seq
+increment by 1 start with 1;
+
+Create sequence discount_seq
+increment by 1 start with 1;
+
+Create sequence staff_seq
+increment by 1 start with 1;
+
+create sequence stockreq_seq
+increment by 1
+start with 1;
+
+create sequence sroot_seq
+increment by 1
+start with 1;
