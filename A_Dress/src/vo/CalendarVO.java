2d1061cd4259b505a66c624f3f7f9d629df9ab87
ama949 <ama949@0214cf69-5816-0510-ba62-bdfd3f077bb4>
2015-07-10 00:25:21 +0000

    VO-CalendarVO

---
+++ A_Dress/src/vo/CalendarVO.java
@@ -0,0 +1,37 @@
+package vo;
+
+public class CalendarVO {
+	private int calen_num;
+	private String calen_start,calen_end, calen_content, calen_date;
+	public int getCalen_num() {
+		return calen_num;
+	}
+	public void setCalen_num(int calen_num) {
+		this.calen_num = calen_num;
+	}
+	public String getCalen_start() {
+		return calen_start;
+	}
+	public void setCalen_start(String calen_start) {
+		this.calen_start = calen_start;
+	}
+	public String getCalen_end() {
+		return calen_end;
+	}
+	public void setCalen_end(String calen_end) {
+		this.calen_end = calen_end;
+	}
+	public String getCalen_content() {
+		return calen_content;
+	}
+	public void setCalen_content(String calen_content) {
+		this.calen_content = calen_content;
+	}
+	public String getCalen_date() {
+		return calen_date;
+	}
+	public void setCalen_date(String calen_date) {
+		this.calen_date = calen_date;
+	}
+	
+}
